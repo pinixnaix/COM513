74d3297706e5d56b0d78b10aed4e8501a88346f6
pinixnaix <91551973+pinixnaix@users.noreply.github.com>
2023-01-31 20:41:55 +0000

    open to read and write txt file

---
+++ 07_file-access.py
@@ -0,0 +1,11 @@
+"""
+activity that open a file and asks the user for more devices to be added to the file
+"""
+
+def run():
+    print("
+        [1]
+    )
+        
+if __name__ == '__main__':
+    run()
