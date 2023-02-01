fcdf890c9dbe9319a9dda69d5cf26f7885e44948
pinixnaix <91551973+pinixnaix@users.noreply.github.com>
2023-02-01 11:31:48 +0000

    added functions to file access artifact

--- 07_file-access.py
+++ 07_file-access.py
@@ -2,10 +2,40 @@
 activity that open a file and asks the user for more devices to be added to the file
 """
 
-def run():
-    print("
-        [1]
-    )
+def menu():
+    """ Function to display a menu"""
+    print("-"*20+" DEVICES "+"-"*20)
+
+    print("\n[1] - Read the devices from the text file"
+          "\n[2] - Add devices to the text file"
+          "\n[3] - Exit")
         
+    try:
+        option = int(input("Please choose an option: "))
+    except ValueError:
+        option = int(input("Error!!! Please enter a valid option: "))
+    except TypeError:
+        option = int(input("Error!!! Please enter a valid option: "))
+    return option
+
+
+def read_file(path):
+    return
+
+def write_file(path):
+    return
+
+
+def run():
+    """Main function"""
+    while True:
+        option = menu()
+        if option == 1:
+            read_file("devices.txt")
+        elif option == 2:
+            write_file("devices.txt")
+        elif option ==3:
+            break
+    
 if __name__ == '__main__':
     run()
