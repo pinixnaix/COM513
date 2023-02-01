85da23b1c7a70145999c41039e2bd174ec0025d1
pinixnaix <91551973+pinixnaix@users.noreply.github.com>
2023-02-01 16:25:12 +0000

    can read and add devices to file

--- 07_file-access.py
+++ 07_file-access.py
@@ -1,15 +1,18 @@
 """
 activity that open a file and asks the user for more devices to be added to the file
 """
+import os
+
 
 def menu():
     """ Function to display a menu"""
+    os.system("clear")
     print("-"*50+"\n"+" "*17+"NETWORK DEVICES \n"+"-"*50)
 
     print("\n[1] - Read the devices from the text file"
           "\n[2] - Add devices to the text file"
           "\n[3] - Exit")
-        
+
     try:
         option = int(input("\nPlease choose an option: "))
     except ValueError:
@@ -21,15 +24,27 @@ def menu():
 
 def read_file(path):
     """Function to read and print the devices text file"""
+    os.system("clear")
     with open(path) as file:
         data = file.read()
         print(data)
+    try:
+        input("Press enter to continue ")
+    except SyntaxError:
+        pass
+    
 
 def write_file(path):
     """Function to add devices to the text file"""
-    with open(path) as file:
-        data = file.read()
-        print(data)
+    os.system("clear")
+    with open(path, "a") as file:
+        while True:
+            new_device = str(input("Enter device name: "))
+            if new_device == "exit":
+                print("\n"+"-"*50+"\n"+" "*17+"All DONE!!!\n"+"-"*50)
+                break
+            else:    
+                file.write(new_device+"\n")
 
 
 def run():
@@ -43,6 +58,7 @@ def run():
         elif option ==3:
             print("\n"+"-"*50+"\n"+" "*17+"THANK YOU!!!\n"+"-"*50)
             break
+
 
 if __name__ == '__main__':
     run()
