8d18ed8384e9edb43f37b7aa8a124c73e25e5c20
pinixnaix <pinixnaix@gmail.com>
2023-02-17 05:21:50 +0000

    update

--- ATLAS-API/router_atlas.py
+++ ATLAS-API/router_atlas.py
@@ -1,7 +1,6 @@
 """
 Program in python to fetch network measurement data from RIPE ATLAS using the API provided.
 """
-import json
 from ripe.atlas.cousteau import AtlasLatestRequest
 from ripe.atlas.cousteau import Measurement
 from ripe.atlas.sagan import PingResult
@@ -10,7 +9,7 @@ from ripe.atlas.sagan import TracerouteResult
 
 def get_measurements():
     """"Function that accepts a measurement ID value from the user """
-    msm_id = input("\nInsert a measurement id: ")
+    msm_id = input("Insert a measurement id: ")
     measurement = Measurement(id=msm_id)
     print("Measurement type: ", measurement.type)
     return msm_id, measurement.type
@@ -20,9 +19,7 @@ def get_ping_results(msm_id):
     """"Function that requests the Ping results """
     kwargs = {"msm_id": msm_id}
     success, results = AtlasLatestRequest(**kwargs).create()
-
     average = PingResult(results[0])
-
     if not success:
         print("\nMeasurement ID not valid!\n")
         run()
@@ -34,32 +31,32 @@ def get_traceroute_results(msm_id):
     """"Function that requests the traceroute results """
     kwargs = {"msm_id": msm_id}
     success, results = AtlasLatestRequest(**kwargs).create()
-
     average = TracerouteResult(results[0])
-
     msm_rtt = []
     for hop in average.hops:
-        msm_rtt.append(hop.median_rtt)
-
+        item = hop.median_rtt
+        if item is not None:
+            msm_rtt.append(item)
     return msm_rtt
 
 
 def run():
     """Main Function"""
-    results = []
-    msm_id, msm_type = get_measurements()
-    if msm_type == 'ping':
-        results.append(get_ping_results(msm_id))
-        while True and len(results) < 3:
-            msm_id, msm_type = get_measurements()
-            if msm_type == 'ping':
-                results.append(get_ping_results(msm_id))
-    elif msm_type == 'traceroute':
-        results = get_traceroute_results(msm_id)
-    #display_results(msmid, results)
-    print(results)
-    print(min(results))
-    print(results.index(min(results)))
+    for costumer in range(3):
+        print("=" * 75)
+        print(f"\t\t\t\t\t\t     Costumer {costumer+1}\n")
+        results = []
+        msm_id, msm_type = get_measurements()
+        if msm_type == 'ping':
+            results.append(get_ping_results(msm_id))
+            while True and len(results) < 3:
+                msm_id, msm_type = get_measurements()
+                if msm_type == 'ping':
+                    results.append(get_ping_results(msm_id))
+        elif msm_type == 'traceroute':
+            results = get_traceroute_results(msm_id)
+
+        print(f"\nStatic Route via ISP{results.index(min(results))+1}\n")
 
 
 if __name__ == "__main__":
